400ee663eb1d57c503801c5006d2f7769603c5cc
Jacob Frick <jacobhfrick1992@gmail.com>
2020-03-03 13:33:18 -0800

    added log

--- Routes/logRoutes.js
+++ Routes/logRoutes.js
@@ -1,8 +1,17 @@
 const router = require('express').Router()
-const {Log} = require('./../models')
+const { Log } = require('./../models')
 
+router.get('/log', (req, res) => {
+    Log.findAll()
+        .then(logs => res.json(logs))
+        .catch(e => console.error(e))
+})
 
-router.post('/log', (req, res))
+router.post('/log', (req, res) => {
+    Log.create(req.body)
+        .then(res.sendStatus(200))
+        .catch(e => console.log(e))
+})
 
 
 module.exports = router
