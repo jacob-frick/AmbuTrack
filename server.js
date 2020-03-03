60aefed46eac754f55907cb2ba6dac4bdf3cf462
Jacob Frick <jacobhfrick1992@gmail.com>
2020-03-03 13:45:35 -0800

    Rename

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-app.use(require('./routes'))
+app.use(require('./Routes'))
 app.get('/', (req, res) => {
   res.render('home')
 })
