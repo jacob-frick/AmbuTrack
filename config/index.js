ada4af5d1bf1abfbef8818898d107e9e572e7784
Jacob Frick <jacobhfrick1992@gmail.com>
2020-03-03 11:40:28 -0800

    maybe fixed db crash

--- config/index.js
+++ config/index.js
@@ -1,3 +1,7 @@
 const Sequelize = require('sequelize')
-const sequelize = new Sequelize(process.env.DB_CON_STRING)
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+    dialect: 'mysql',
+    host: process.env.DB_HOST,
+    port: 3306
+  })
 module.exports = sequelize
