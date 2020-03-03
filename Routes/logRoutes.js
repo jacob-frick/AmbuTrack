5afa57d627513e8aaaaa2c6f0c4153bf05260bde
Jacob Frick <jacobhfrick1992@gmail.com>
2020-03-03 14:10:25 -0800

    updated routes

--- Routes/logRoutes.js
+++ Routes/logRoutes.js
@@ -13,5 +13,16 @@ router.post('/log', (req, res) => {
         .catch(e => console.log(e))
 })
 
-
+router.get('/log/:id', (req, res) => {
+    Log.findOne({ where: { id: req.params.id } })
+        .then(log => res.json(log))
+        .catch(e => console.error(e))
+})
+     
+// DELETE a user by ID
+router.delete('/log/:id', (req, res) => {
+    Log.destroy({ where: { id: req.params.id } })
+      .then(() => res.sendStatus(200))
+      .catch(e => console.error(e))
+  })
 module.exports = router
