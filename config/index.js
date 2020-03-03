f36f52921ce08f42caea570d6fa5e5f532bb582f
Jacob Frick <jacobhfrick1992@gmail.com>
2020-03-03 11:49:49 -0800

    db should work

--- config/index.js
+++ config/index.js
@@ -1,3 +1,12 @@
 const Sequelize = require('sequelize')
-const sequelize = new Sequelize(process.env.JAWSDB_URL)
+let sequelize
+if (process.env.JAWSDB_URL) {
+    sequelize =new Sequelize(process.JAWSDB_URL)
+} else {
+    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+        dialect: 'mysql',
+        host: process.env.DB_HOST,
+        port: 3306
+    })
+}
 module.exports = sequelize
