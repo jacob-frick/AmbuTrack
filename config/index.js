fa696e1440c8562b5554c0c362f497dba15b8e05
Jacob Frick <jacobhfrick1992@gmail.com>
2020-03-03 11:50:26 -0800

    jacob dumb

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize')
 let sequelize
 if (process.env.JAWSDB_URL) {
-    sequelize =new Sequelize(process.JAWSDB_URL)
+    sequelize =new Sequelize(process.env.JAWSDB_URL)
 } else {
     sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
         dialect: 'mysql',
