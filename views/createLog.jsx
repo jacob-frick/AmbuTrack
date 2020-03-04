a0d21b6832ccbd883a93ec0a5e9f75aa70da6a69
Jacob Frick <jacobhfrick1992@gmail.com>
2020-03-04 11:06:46 -0800

    added more page routes and main redirect

---
+++ views/createLog.jsx
@@ -0,0 +1,12 @@
+const React = require('react')
+const Html = require('./layouts/default.jsx')
+
+const CreateLog = props => {
+    return (
+        <Html>
+            <h1>Create Log</h1>
+        </Html>
+    )
+}
+
+module.exports = CreateLog
