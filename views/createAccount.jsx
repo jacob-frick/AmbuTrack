45c6048e9ceb37c63ec616046bcbaba837561313
Frank Savage <gotsavaged@gmail.com>
2020-03-04 14:08:06 -0800

    add btn styles to create acct page

--- views/createAccount.jsx
+++ views/createAccount.jsx
@@ -35,7 +35,7 @@ const CreateAccount = props => {
                 <label htmlFor="accountCreateEmployer">Employer Name</label>
                 <input type="Username" className="form-control" id="accountCreateUserName" />
               </div>
-              <button type="submit" className="btn btn-primary">Submit</button>
+              <button type="submit" className="btn blue-gradient ctrl-standard fx-bubbleDown ">Submit</button>
             </form>
           </div>
         </div>
