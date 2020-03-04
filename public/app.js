02fce17e526019710c9f74c12160db7cc16b51fa
guidoasbun <gdrodrigo8@gmail.com>
2020-03-04 10:53:37 -0800

    create log function in app.js

--- public/app.js
+++ public/app.js
@@ -1,1 +1,21 @@
 console.log('ping')
+
+const { axios, localStorage } = window
+
+let uid = localStorage.getItem('uid')
+let uname = localStorage.getItem('uname')
+
+
+
+
+const createLog = () => {
+  axios.post('api/logs', {
+    date: document.getElementById('date').value,
+    holdWall: document.getElementById('holdWall').value,
+    bls: document.getElementById('bls').value,
+    als: document.getElementById('als').value,
+    calls: document.getElementById('calls').value,
+    cancels: document.getElementById('cancels').value
+  })
+}
+
