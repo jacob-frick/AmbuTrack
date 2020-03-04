c13d29383cb2574dc3c513c683abc5f41f49652c
Jacob Frick <jacobhfrick1992@gmail.com>
2020-03-04 13:19:30 -0800

    updated dashboard

--- routes/pageRoutes.js
+++ routes/pageRoutes.js
@@ -1,21 +1,26 @@
 const router = require('express').Router()
-
+const {User} = require('./../models')
 router.get('/', (req, res) => {
     res.render('home')
 })
-router.get('/createAccount', (req, res) =>{
+router.get('/createAccount', (req, res) => {
     res.render('createAccount')
 })
-router.get('/dashboard/:uid', (req,res)=>{
-    res.render('dashboard', {uid: req.params.uid})
+router.get('/dashboard/:uid', (req, res) => {
+    User.findOne({ where: { id: req.params.uid } })
+    .then(users => {
+        res.render('dashboard', JSON.parse(JSON.stringify(users)))
+    })
+    .catch(e => console.error(e))
 })
-router.get('/profile/:uid', (req,res)=> {
-    res.render('profile', {uid:req.params.uid})
+
+router.get('/profile/:uid', (req, res) => {
+    res.render('profile', { uid: req.params.uid })
 })
-router.get('/createLog', (req,res) =>{
+router.get('/createLog', (req, res) => {
     res.render('createLog')
 })
-router.get('/**', (req,res)=>{
+router.get('/**', (req, res) => {
     res.redirect('/')
 })
 module.exports = router
