ccb9379791f4bea187d283f82ffd3fbf796d4c35
Jacob Frick <jacobhfrick1992@gmail.com>
2020-03-04 11:15:06 -0800

    removed routes

--- Routes/index.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router()
-
-router.use('/api', require('./logRoutes.js'))
-router.use('/api', require('./userRoutes.js'))
-router.use('/', require('./pageRoutes.js'))
-module.exports = router
