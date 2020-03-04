2f493a422522663ce286f4153dde41fa8b8c90a4
Jacob Frick <jacobhfrick1992@gmail.com>
2020-03-04 13:51:06 -0800

    updated form component

--- views/createLog.jsx
+++ views/createLog.jsx
@@ -1,40 +1,18 @@
 const React = require('react')
 const Html = require('./layouts/default.jsx')
-
+const FormItem = require('./components/formItem.jsx')
 const CreateLog = props => {
     return (
         <Html>
         <form>
-            <div class="form-group">
-                <label for="exampleInputEmail1">Enter Date</label>
-                <input type="text" class="form-control" id="date" aria-describedby="emailHelp"></input>
-            </div>
-            <div class="form-group">
-                <label for="exampleInputEmail1">Enter hold wall</label>
-                <input type="text" class="form-control" id="holdWall" aria-describedby="emailHelp"></input>
-                
-            </div>
-            <div class="form-group">
-                <label for="exampleInputEmail1">Enter bls</label>
-                <input type="text" class="form-control" id="bls" aria-describedby="emailHelp"></input>
-                
-            </div>
-            <div class="form-group">
-                <label for="exampleInputEmail1">Enter als</label>
-                <input type="text" class="form-control" id="als" aria-describedby="emailHelp"></input>
-                
-            </div>
-            <div class="form-group">
-                <label for="exampleInputEmail1">Enter calls</label>
-                <input type="text" class="form-control" id="calls" aria-describedby="emailHelp"></input>
-                
-            </div>
-            <div class="form-group">
-                <label for="exampleInputEmail1">Enter cancels</label>
-                <input type="text" class="form-control" id="cancels" aria-describedby="emailHelp"></input>
-                
-            </div>
-        <button id="createLog" type="submit" class="btn btn-primary">Submit</button>
+            <FormItem title='Enter Date' id='date' forItem='date' desc='dateInput'/>
+            <FormItem title='Minutes Holding Wall' id='holdWall' forItem='holdWall' desc='minsHoldWall'/>
+            <FormItem title='Enter times BLS' id='bls' forItem='bls' desc='blsInput' />
+            <FormItem title='Enter times ALS' id='als' forItem='als' desc='alsInput'/>
+            <FormItem title='Enter total number of calls' id='calls' forItem='calls' desc='callsInput'/>
+            <FormItem title='Enter cancel times' id='cancels' forItem='cancels' desc='cancelsInput'/>
+
+        <button id="createLog" type="submit" className="btn btn-primary">Submit</button>
         </form>
         <script src="./scripts/createLog.js"></script>
         </Html>
