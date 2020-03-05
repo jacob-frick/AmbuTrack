5c24f0f1547ccceea2fc51e33ab81531d46ffb2f
Julian <Van Ost>
2020-03-05 13:01:30 -0800

    removes app.js reference from default.jsx

--- views/layouts/default.jsx
+++ views/layouts/default.jsx
@@ -19,7 +19,6 @@ const Html = props => {
         <script src="https://cdnjs.cloudflare.com/ajax/libs/d3/5.15.0/d3.js"></script>
         <script src='https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js' integrity='sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo' crossOrigin='anonymous'></script>
         <script src='https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js' integrity='sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6' crossOrigin='anonymous'></script>
-        <script src='app.js'></script>
       </body>
     </html>
   )
