ab0159045b6bb853a9ca102fd77d60fe24fa071e
Julian <Van Ost>
2020-03-05 12:18:06 -0800

    fixes naming standard for main style-overrides.css file and its reference in default.jsx, was named login.css, changed to style-overrides.css

--- views/layouts/default.jsx
+++ views/layouts/default.jsx
@@ -9,7 +9,7 @@ const Html = props => {
         <title>AmbuTrack</title>
         <link rel="stylesheet" type="text/css" href="https://ilt-insightdlp-static.s3.amazonaws.com/css/default.css"></link>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossOrigin="anonymous"></link>
-        <link rel="stylesheet" type="text/css" href="css/login.css"></link>
+        <link rel="stylesheet" type="text/css" href="css/style-overrides.css"></link>
         <link rel="shortcut icon" href="images/favicon.png" />
       </head>
       <body>
