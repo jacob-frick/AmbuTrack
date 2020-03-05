59ffba4053e4ce437e701cabfa0a932be3035c31
guidoasbun <gdrodrigo8@gmail.com>
2020-03-05 12:21:59 -0800

    fixed app.js

--- public/app.js
+++ public/app.js
@@ -4,21 +4,3 @@ const { axios, localStorage } = window
 
 let uid = localStorage.getItem('uid')
 let uname = localStorage.getItem('uname')
-
-
-
-
-
-// const createUser = () => {
-//   axios.post('/api/user', {
-//     firstName: document.getElementById('firstName').value,
-//     lastName: document.getElementById('lastName').value,
-//     usename: document.getElementById('usename').value,
-//     email: document.getElementById('email').value,
-//     employer: document.getElementById('employer').value
-//   })
-// }
-
-
-
-
