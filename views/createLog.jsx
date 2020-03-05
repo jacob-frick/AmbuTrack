10527ea04ab5cf48b99c6a8e85e51b61e9364437
Frank Savage <gotsavaged@gmail.com>
2020-03-05 13:34:05 -0800

    changes to log page

--- views/createLog.jsx
+++ views/createLog.jsx
@@ -4,13 +4,29 @@ const FormItem = require('./components/formItem.jsx')
 const CreateLog = props => {
     return (
         <Html>
+            <div className="container outerContainer">
+                <div className="row">
+                    <h1 className="display-4 headerAmbutrack">AMBUTRACK</h1>
+                    <div className="jumbotron col-md-12">
+                        <span className="gridContents">
+                            <h2>Shift Logs</h2>
+                            <img className="createAccountLifeStarImg" src="images/lifeStar.png"></img>
+                        </span>
+                        <form>
+                            <div className="form-group"></div>
+                        </form>
+            </div>
+        </div>
+    </div>
+
         <form>
-            <FormItem title='Enter Date' id='date' forItem='date' desc='dateInput'/>
-            <FormItem title='Minutes Holding Wall' id='holdWall' forItem='holdWall' desc='minsHoldWall'/>
-            <FormItem title='Enter times BLS' id='bls' forItem='bls' desc='blsInput' />
-            <FormItem title='Enter times ALS' id='als' forItem='als' desc='alsInput'/>
-            <FormItem title='Enter total number of calls' id='calls' forItem='calls' desc='callsInput'/>
-            <FormItem title='Enter cancel times' id='cancels' forItem='cancels' desc='cancelsInput'/>
+            
+                <FormItem title='City' id='city' forItem='city' desc='cityCall' />
+                <FormItem title='Minutes Holding Wall' id='holdWall' forItem='holdWall' desc='minsHoldWall'/>
+                <FormItem title='Enter times BLS' id='bls' forItem='bls' desc='blsInput' />
+                <FormItem title='Enter times ALS' id='als' forItem='als' desc='alsInput'/>
+                <FormItem title='Enter total number of calls' id='calls' forItem='calls' desc='callsInput'/>
+                <FormItem title='Enter cancel times' id='cancels' forItem='cancels' desc='cancelsInput'/>
 
         <button id="createLog" type="submit" className="btn btn-primary">Submit</button>
         </form>
