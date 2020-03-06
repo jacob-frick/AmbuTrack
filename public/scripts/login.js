2ac6afd7c59844b3490925a1f15332274858a9e4
guidoasbun <gdrodrigo8@gmail.com>
2020-03-05 21:26:41 -0800

    working on login.js

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -11,9 +11,22 @@ document.getElementById('signIn').addEventListener('click', event => {
   event.preventDefault()
   let userName = document.getElementById('userName').value
   let email = document.getElementById('email').value
+
+    axios.get(`/api/user/username/${userName}`)
+      .then
+
+    // .then(({ data: [{ firstName, username }] }) => {
+    //   fName = firstName
+    //   uName = username
+    //   localStorage.setItem('fName', firstName)
+    //   localStorage.setItem('uname', username)
+    // })
   console.log(userName)
   console.log(email)
   
-})
+  // window.location.href = '/dashboard'
+  
+  })
 
-console.log('ping')
+
+
