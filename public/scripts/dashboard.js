63e2b0b153c0d53fde65afd40d13a0a8d29876eb
guidoasbun <gdrodrigo8@gmail.com>
2020-03-06 13:18:46 -0800

    added comments tp dashboard.js

--- public/scripts/dashboard.js
+++ public/scripts/dashboard.js
@@ -1,8 +1,10 @@
 const firstName = localStorage.getItem('firstName')
 const lastName = localStorage.getItem('lastName')
+// gets userid from local storage passed down from login
 const id = localStorage.getItem('id')
+//initializes logData array
 let logData = []
-
+//pulls all of the log data from the userId passed down from login
 axios.get(`/api/log/user/${id}`)
   .then((data) => {
     console.log(data)
