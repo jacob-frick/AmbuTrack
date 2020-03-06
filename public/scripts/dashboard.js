e8c58b25c2c6fc2ce3b0dc93dbc03f8f3aa6f69e
guidoasbun <gdrodrigo8@gmail.com>
2020-03-06 10:33:39 -0800

    created local storage pull of id on dachboard.js

--- public/scripts/dashboard.js
+++ public/scripts/dashboard.js
@@ -1,5 +1,6 @@
 const firstName = localStorage.getItem('firstName')
 const lastName = localStorage.getItem('lastName')
+const id = localStorage.getItem('id')
 
 
 document.getElementById('name').innerHTML = (`${firstName} ${lastName}`)
