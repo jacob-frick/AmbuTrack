fb6fe04a4d2e5cf8eb5e0663bc3a8d01f78e7dfe
guidoasbun <gdrodrigo8@gmail.com>
2020-03-06 13:49:06 -0800

    changes to dashboard.js

--- public/scripts/dashboard.js
+++ public/scripts/dashboard.js
@@ -4,11 +4,16 @@ const lastName = localStorage.getItem('lastName')
 const id = localStorage.getItem('id')
 //initializes logData array
 let logData = []
-//pulls all of the log data from the userId passed down from login
-axios.get(`/api/log/user/${id}`)
-  .then((data) => {
-    console.log(data)
-    logData = data
-    console.log(logData)
-  })
 
+const renderLog = _=> {
+  axios.get(`/api/log/user/${id}`)
+    .then((data) => {
+      console.log(data)
+      logData = data.data
+      console.log(logData)
+    })
+}
+
+renderLog()
+
+  
