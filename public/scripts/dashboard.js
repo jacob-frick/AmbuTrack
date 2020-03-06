2e08ac66b3e47d6c22bc72d0ea76395af93fc930
guidoasbun <gdrodrigo8@gmail.com>
2020-03-06 10:20:28 -0800

    deleted console.logs

--- public/scripts/dashboard.js
+++ public/scripts/dashboard.js
@@ -1,6 +1,5 @@
 const firstName = localStorage.getItem('firstName')
 const lastName = localStorage.getItem('lastName')
-console.log(firstName)
-console.log(lastName)
+
 
 document.getElementById('name').innerHTML = (`${firstName} ${lastName}`)
