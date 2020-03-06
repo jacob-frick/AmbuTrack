c9a91aba1a13d233349892dd623d0aae8b4c30bf
Frank Savage <gotsavaged@gmail.com>
2020-03-06 14:51:27 -0800

    created props for  logCard.jsx

--- views/dashboard.jsx
+++ views/dashboard.jsx
@@ -2,6 +2,7 @@ const React = require('react')
 const Html = require('./layouts/default.jsx')
 const Navbar = require('./components/navbar.jsx')
 const SideBar = require('./components/sidebar.jsx')
+const LogCard = require('./components/logCard.jsx')
 const Dashboard = props => {
     return (
         <Html>
@@ -12,26 +13,7 @@ const Dashboard = props => {
                 <div id="accordion">
                     {props.log.map((elem, index) => {
                         return (
-                            <div className="card w-75">
-
-                                <a className="" data-toggle="collapse" href={`#collapse${index}`} role="button" aria-expanded="false" aria-controls="collapseExample">
-                                    <div className="card-header" id="headingOne">
-                                        <h5 className="mb-0">
-                                            {elem.date}{elem.location}
-                                        </h5>
-                                    </div>
-                                </a>
-                                <div id={`collapse${index}`} className="collapse" aria-labelledby="headingOne" data-parent="#accordion">
-                                    <div className="card-body">
-                                        <a href="#" class="list-group-item list-group-item-action list-group-item-action">Time spent holding the wall: {elem.holdWall}</a>
-                                        <a href="#" class="list-group-item list-group-item-action list-group-item-action">BLS Count: {elem.bls}</a>
-                                        <a href="#" class="list-group-item list-group-item-action list-group-item-action">ALS Count: {elem.als}</a>
-                                        <a href="#" class="list-group-item list-group-item-action list-group-item-action">Canceled calls: {elem.cancels}</a>
-                                        <a href="#" class="list-group-item list-group-item-action list-group-item-action">Total calls: {elem.bls + elem.als + elem.cancels}</a>
-                                        <a href="#" class="btn btn-danger btn-xsm float-right mt-3 mb-3" type="submit">Delete</a>
-                                    </div>
-                                </div>
-                            </div>
+                            <LogCard elem={elem} index={index} />
                         )
                     })}
                 </div>
