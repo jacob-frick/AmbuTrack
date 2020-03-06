1cbb7771d75fdaab5d0ab74b1a9b78cbcb335254
Jacob Frick <jacobhfrick1992@gmail.com>
2020-03-06 10:12:11 -0800

    commit

--- views/components/navbar.jsx
+++ views/components/navbar.jsx
@@ -1,24 +1,21 @@
 const React = require('react')
 const Navbar = props => {
   return (
-    <nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
-      <a className='navbar-brand' href='#'>Navbar</a>
-      <button className='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation'>
-        <span className='navbar-toggler-icon' />
+    <nav className="navbar navbar-expand-sm sticky-top navbar-dark bg-dark flex-md-nowrap">
+      <a className="navbar-brand" href="#">Navbar</a>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
       </button>
-      <div className='collapse navbar-collapse' id='navbarSupportedContent'>
-        <ul className='navbar-nav mr-auto'>
-          <li className='nav-item active'>
-            <a className='nav-link' href={'/dashboard/' + props.uid}>Home <span className='sr-only'>(current)</span></a>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav d-sm-none">
+          <li className="nav-item active">
+            <a className="nav-link" href="#">Dashboard</a>
           </li>
-          <li className='nav-item'>
-            <a className='nav-link' href='/createLog'>Create New Log</a>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Profile</a>
           </li>
-          <li className='nav-item'>
-            <a className='nav-link' href='/profile'>View Profile</a>
-          </li>
-          <li className='nav-item float-right'>
-            <a className='nav-link' href='/login'>Sign Out</a>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Create Log</a>
           </li>
         </ul>
       </div>
---
+++ views/components/sidebar.jsx
@@ -0,0 +1,23 @@
+const React = require('react')
+
+const SideBar = props => {
+    return (
+        <div className="container-fluid">
+            <div className="row">
+                <nav className="col-sm-3 col-lg-2 d-none d-sm-block bg-light sidebar">
+                    <div className="sidebar-sticky">
+                        <ul className="nav flex-column mt-3">
+                            <li className="nav-item"><a className="nav-link" href="#">Dashboard</a></li>
+                            <li className="nav-item"><a className="nav-link" href="#">Profile</a></li>
+                            <li className="nav-item"><a className="nav-link" href="#">Create Log</a></li>
+                        </ul>
+                    </div>
+                </nav>
+                <main role="main" className="col-sm-9 ml-auto col-lg-10 pt-3 px-4">
+                        {props.children}
+                </main>
+            </div>
+        </div>
+    )
+}
+module.exports = SideBar
