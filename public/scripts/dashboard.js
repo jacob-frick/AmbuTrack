ae28a111b9e8061045858beb1916693c65f3fb93
guidoasbun <gdrodrigo8@gmail.com>
2020-03-07 18:25:27 -0800

    worked on login.js authentication

--- public/scripts/dashboard.js
+++ public/scripts/dashboard.js
@@ -1,19 +1,18 @@
 const firstName = localStorage.getItem('firstName')
 const lastName = localStorage.getItem('lastName')
-// gets userid from local storage passed down from login
 const id = localStorage.getItem('id')
+// gets userid from local storage passed down from login
+// const id = localStorage.getItem('id')
 //initializes logData array
-let logData = []
+// let logData = []
 
-const renderLog = _=> {
-  axios.get(`/api/log/user/${id}`)
-    .then((data) => {
-      console.log(data)
-      logData = data.data
-      console.log(logData)
-    })
-}
+// const renderLog = _=> {
+//   axios.get(`/api/log/user/${id}`)
+//     .then((data) => {
+//       console.log(data)
+//       logData = data.data
+//       console.log(logData)
+//     })
+// }
+// renderLog()
 
-renderLog()
-
-  
