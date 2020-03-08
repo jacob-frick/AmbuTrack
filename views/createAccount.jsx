9164555e27e27dfabfaed1c2547d60a2be701b6d
Jacob Frick <jacobhfrick1992@gmail.com>
2020-03-07 19:14:30 -0800

    fixed overflow problem on login/createAccount

--- views/createAccount.jsx
+++ views/createAccount.jsx
@@ -2,48 +2,50 @@ const React = require('react')
 const Html = require('./layouts/default.jsx')
 const CreateAccount = props => {
   return (
-    <Html bclass='b-login'>
-      <div className="container outerContainer">
-        <div className="row">
-          <img className="ambutrackHeaderLogo" src="images/customLogoOne.png"></img>
-          <div className="jumbotron col-md-12">
-            <span className="gridContents"></span>
-            <form>
-              <div className="form-group">
-                <div className="row">
-                  <div className="col">
-                    <label htmlFor="firstName">First Name</label>
-                    <input id="firstName" type="text" className="form-control" placeholder="First Name" />
-                  </div>
-                  <div className="col">
-                    <label htmlFor="lastName">Last Name</label>
-                    <input id="lastName" type="text" className="form-control" placeholder="Last Name" />
-                  </div>
-                </div>
+    <Html>
+      <div className="b-login">
+        <div className="container outerContainer">
+          <div className="row">
+            <img className="ambutrackHeaderLogo" src="images/customLogoOne.png"></img>
+            <div className="jumbotron col-md-12">
+              <span className="gridContents"></span>
+              <form>
                 <div className="form-group">
                   <div className="row">
                     <div className="col">
-                      <label htmlFor="email">Email Address</label>
-                      <input type="email" className="form-control" id="email" aria-describedby="emailHelp" />
+                      <label htmlFor="firstName">First Name</label>
+                      <input id="firstName" type="text" className="form-control" placeholder="First Name" />
                     </div>
                     <div className="col">
-                      <label htmlFor="username">Username</label>
-                      <input type="text" className="form-control" id="username" />
+                      <label htmlFor="lastName">Last Name</label>
+                      <input id="lastName" type="text" className="form-control" placeholder="Last Name" />
                     </div>
                   </div>
-                  <small id="emailHelp" className="form-text text-muted">We will share your email with everyone we can!!</small>
-                </div>
-                <div className="form-group">
-                  <div className="col">
+                  <div className="form-group">
                     <div className="row">
-                      <label htmlFor="employer">Employer Name</label>
-                      <input type="text" className="form-control" id="employer" />
+                      <div className="col">
+                        <label htmlFor="email">Email Address</label>
+                        <input type="email" className="form-control" id="email" aria-describedby="emailHelp" />
+                      </div>
+                      <div className="col">
+                        <label htmlFor="username">Username</label>
+                        <input type="text" className="form-control" id="username" />
+                      </div>
+                    </div>
+                    <small id="emailHelp" className="form-text text-muted">We will share your email with everyone we can!!</small>
+                  </div>
+                  <div className="form-group">
+                    <div className="col">
+                      <div className="row">
+                        <label htmlFor="employer">Employer Name</label>
+                        <input type="text" className="form-control" id="employer" />
+                      </div>
                     </div>
                   </div>
+                  <a href="#home"><button type="submit" className="btn btn-hover aqua-gradient float-right" id="btnCreateUser">Submit</button></a>
                 </div>
-                <a href="#home"><button type="submit" className="btn btn-hover aqua-gradient float-right" id="btnCreateUser">Submit</button></a>
-              </div>
-            </form>
+              </form>
+            </div>
           </div>
         </div>
       </div>
--- views/home.jsx
+++ views/home.jsx
@@ -7,12 +7,14 @@ const List = require('./components/list.jsx')
 const Jumbotron = require('./components/account.jsx')
 
 const Home = props => {
-  return (
-    <Html bclass='b-login'>
-      <Login />
-      <script src="./scripts/login.js"></script>
-    </Html>
-  )
+    return (
+        <Html>
+            <div className='b-login'>
+                <Login />
+                <script src="./scripts/login.js"></script>
+            </div>
+        </Html>
+    )
 }
 
 module.exports = Home
