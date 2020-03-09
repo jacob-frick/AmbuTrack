56d088511b6eeb6e4a04af089bdcabde42bfc322
Jacob Frick <jacobhfrick1992@gmail.com>
2020-03-09 14:18:58 -0700

    added pdf gen and download

---
+++ routes/downloadRoutes.js
@@ -0,0 +1,76 @@
+const router = require('express').Router()
+const { User, Log } = require('../models')
+const PDFDocument = require('pdfkit');
+const fs = require('fs');
+const blobStream  = require('blob-stream');
+
+function formatLogs(logs) {
+    let formatLogs = []
+    logs.forEach(elem => {
+        formatLogs.push([
+            `Date: ${elem.date}          Location: ${elem.location}`,
+            [
+                `Time spent holding wall: ${elem.holdWall}`,
+                `BLS Count: ${elem.bls}`,
+                `ALS Count: ${elem.als}`,
+                `Cancel Count: ${elem.cancels}`,
+                `Total Count: ${elem.bls + elem.als + elem.cancels}`,
+                ''
+            ]
+        ])
+    })
+    return formatLogs
+}
+function sumAllCalls(log) {
+    let count = { bls: 0, holdWall: 0, als: 0, cancels: 0 }
+    log.forEach(elem => {
+        count.bls += elem.bls
+        count.als += elem.als
+        count.holdWall += elem.holdWall
+        count.cancels += elem.cancels
+    })
+    return count
+}
+router.get('/download/:uid', (req, res) => {
+
+    // Create a document
+    const doc = new PDFDocument();
+    User.findOne({ where: { id: req.params.uid } })
+        .then(user => {
+            const userData = JSON.parse(JSON.stringify(user))
+            Log.findAll({ where: { userId: req.params.uid } })
+                .then(logs => {
+                    const log = JSON.parse(JSON.stringify(logs))
+                    doc.pipe(fs.createWriteStream(`${__dirname}/../userGenPDFs/${userData.username}.pdf`));
+                    doc.moveDown()
+                    doc.image(`${__dirname}/../public/images/customLogoOne.png`, 50, 15, { width: 500, align: 'right' })
+                    const formattedLogs = formatLogs(log)
+                    const count = sumAllCalls(log)
+                    doc.moveDown()
+                    doc.fontSize(20)
+                    doc.text(`This PDF contains all the user data for ${userData.firstName} ${userData.lastName}`)
+                    doc.text('')
+                    doc.moveDown()
+                    doc.fontSize(16)
+                    doc.text(`Email: ${userData.email}`)
+                    doc.text(`Employer: ${userData.employer}`)
+                    doc.text(`Username: ${userData.username}`)
+                    doc.text(`Total logs submitted: ${log.length}`)
+                    doc.text(`Total Calls: ${count.als + count.bls + count.cancels}`)
+                    doc.text(`Total ALS Count: ${count.als}`)
+                    doc.text(`Total BLS Count: ${count.bls}`)
+                    doc.text(`Total Cancels: ${count.cancels}`)
+                    doc.text(`Total Minutes Holding the Wall: ${count.holdWall}`)
+                    doc.text('')
+                    doc.moveDown()
+                    doc.fontSize(12)
+                    doc.list(formattedLogs, { listType: 'bulletRadius', align: 'left' })
+                    doc.end();
+                })
+                .then(res.download(`${__dirname}/../userGenPDFs/${userData.username}.pdf`))
+        })
+    // Pipe its output somewhere, like to a file or HTTP response
+    // See below for browser usage
+
+})
+module.exports = router
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 
 router.use('/api', require('./logRoutes.js'))
 router.use('/api', require('./userRoutes.js'))
+router.use('/api', require('./downloadRoutes.js'))
 router.use('/', require('./pageRoutes.js'))
 router.get('/**', (req, res) => {
     res.redirect('/')
