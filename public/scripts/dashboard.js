de7d1af5b05f170b6ce24e0c1d89d3474aff5099
Jacob Frick <jacobhfrick1992@gmail.com>
2020-03-09 01:15:56 -0700

    log delete functionality done

--- public/scripts/dashboard.js
+++ public/scripts/dashboard.js
@@ -15,20 +15,24 @@
 //     })
 // }
 // renderLog()
-document.addEventListener('click',({target})=>{
+document.addEventListener('click', ({ target }) => {
     const reg = /lc-btn-[0-9]*/
-    if(reg.test(target.id)){
+    if (reg.test(target.id)) {
         const index = parseInt((target.id).replace('lc-btn-', ''))
-        document.getElementById(`log-card-${index}`).style.opacity = 1
-        setInterval(() => {
-            if(document.getElementById(`log-card-${index}`).style.opacity <= 0){
-                document.getElementById(`log-card-${index}`).style.display='none'
-                clearInterval()
-            } else{
-                document.getElementById(`log-card-${index}`).style.opacity-=0.01
-            }
-        }, 2)
-
+        axios.delete(`/api/log/${index}`)
+        .then((res)=> {
+            console.log(res)
+            document.getElementById(`log-card-${index}`).style.cursor = 'wait'
+            document.getElementById(`log-card-${index}`).style.opacity = 1
+            setInterval(() => {
+                if(document.getElementById(`log-card-${index}`).style.opacity <= 0){
+                    document.getElementById(`log-card-${index}`).style.display='none'
+                    clearInterval()
+                } else{
+                    document.getElementById(`log-card-${index}`).style.opacity-=0.01
+                }
+            }, 2)
+        })
     }
 })
 
