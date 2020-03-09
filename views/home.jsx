e3fe45c584916ae8a4ce61f0a3f253477f3549f7
Julian <Van Ost>
2020-03-09 13:40:03 -0700

    File cleanup, attempted favicon fix for other views... :-(

--- views/home.jsx
+++ views/home.jsx
@@ -3,9 +3,6 @@ const Html = require('./layouts/default.jsx')
 const Navbar = require('./components/navbar.jsx')
 const Login = require('./components/login.jsx')
 
-const List = require('./components/list.jsx')
-const Jumbotron = require('./components/account.jsx')
-
 const Home = props => {
     return (
         <Html>
