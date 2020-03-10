ad0340944663cae00954fc494a47ca2a78bab4b9
guidoasbun <gdrodrigo8@gmail.com>
2020-03-09 23:52:51 -0700

    fixes

--- public/scripts/profile.js
+++ public/scripts/profile.js
@@ -1,14 +1,9 @@
 console.log('hello')
 const id = localStorage.getItem('id')
 
-
 axios.get(`/api/log/user/${id}`)
   .then((data) => {
-    let totalLogs
-    console.log(data)
-    for (i = 0; i <= data.data.length; i++){
-    totalLogs = i 
-    }
+    const totalLogs = data.dada.length
     document.getElementById('totalLogs').innerHTML = totalLogs
     console.log(totalLogs)
   })
