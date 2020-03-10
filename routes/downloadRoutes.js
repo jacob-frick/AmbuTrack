c61f4cea5f2c756385d8d7cb7e3527b36b18e848
Jacob Frick <jacobhfrick1992@gmail.com>
2020-03-09 18:42:48 -0700

    another deploy test

--- routes/downloadRoutes.js
+++ routes/downloadRoutes.js
@@ -31,7 +31,7 @@ function sumAllCalls(log) {
     })
     return count
 }
-router.get('/pdf/:uid', (req, res) => {
+router.get('/:uid', (req, res) => {
 
     // Create a document
     const doc = new PDFDocument();
@@ -41,45 +41,43 @@ router.get('/pdf/:uid', (req, res) => {
             fs.writeFileSync(`${__dirname}/../public/userGenPDFs/${userData.username}.pdf`)
             Log.findAll({ where: { userId: req.params.uid } })
                 .then(logs => {
-                    res.render('displayPDF', {userData})
-
-                    // const log = JSON.parse(JSON.stringify(logs))
-                    // let ws = fs.createWriteStream(`${__dirname}/../public/userGenPDFs/${userData.username}.pdf`)
-                    // doc.pipe(ws)
-                    // doc.info.Title=`${userData.username}.pdf`
-                    // doc.moveDown()
-                    // doc.image(`${__dirname}/../public/images/customLogoOne.png`, 50, 15, { width: 500, align: 'right' })
-                    // const formattedLogs = formatLogs(log)
-                    // const count = sumAllCalls(log)
-                    // doc.moveDown()
-                    // doc.fontSize(20)
-                    // doc.text(`This PDF contains all the user data for ${userData.firstName} ${userData.lastName}`)
-                    // doc.text('')
-                    // doc.moveDown()
-                    // doc.fontSize(16)
-                    // doc.text(`Email: ${userData.email}`)
-                    // doc.text(`Employer: ${userData.employer}`)
-                    // doc.text(`Username: ${userData.username}`)
-                    // doc.text(`Total logs submitted: ${log.length}`)
-                    // doc.text(`Total Calls: ${count.als + count.bls + count.cancels}`)
-                    // doc.text(`Total ALS Count: ${count.als}`)
-                    // doc.text(`Total BLS Count: ${count.bls}`)
-                    // doc.text(`Total Cancels: ${count.cancels}`)
-                    // doc.text(`Total Minutes Holding the Wall: ${count.holdWall}`)
-                    // doc.text('')
-                    // doc.moveDown()
-                    // doc.fontSize(12)
-                    // doc.list(formattedLogs, { listType: 'bulletRadius', align: 'left' })
-                    // doc.end()
-                    // ws.on('finish', () => {
-                    //     // fs.readFile(`${__dirname}/../userGenPDFs/${userData.username}.pdf`, function (err, data) {
-                    //     //     res.contentType('application/pdf');
-                    //     //     res.setHeader('Content-disposition', `inline; filename="${userData.username}"`);
-                    //     //     res.send(data);
-                    //     // })
-                    //     res.render('displayPDF', {userData, filepath: `${__dirname}/../public/userGenPDFs/${userData.username}.pdf`})
-                    // })
-                    // //res.sendfile(`${__dirname}/../userGenPDFs/${userData.username}.pdf`)
+                    const log = JSON.parse(JSON.stringify(logs))
+                    let ws = fs.createWriteStream(`${__dirname}/../public/userGenPDFs/${userData.username}.pdf`)
+                    doc.pipe(ws)
+                    doc.info.Title=`${userData.username}.pdf`
+                    doc.moveDown()
+                    doc.image(`${__dirname}/../public/images/customLogoOne.png`, 50, 15, { width: 500, align: 'right' })
+                    const formattedLogs = formatLogs(log)
+                    const count = sumAllCalls(log)
+                    doc.moveDown()
+                    doc.fontSize(20)
+                    doc.text(`This PDF contains all the user data for ${userData.firstName} ${userData.lastName}`)
+                    doc.text('')
+                    doc.moveDown()
+                    doc.fontSize(16)
+                    doc.text(`Email: ${userData.email}`)
+                    doc.text(`Employer: ${userData.employer}`)
+                    doc.text(`Username: ${userData.username}`)
+                    doc.text(`Total logs submitted: ${log.length}`)
+                    doc.text(`Total Calls: ${count.als + count.bls + count.cancels}`)
+                    doc.text(`Total ALS Count: ${count.als}`)
+                    doc.text(`Total BLS Count: ${count.bls}`)
+                    doc.text(`Total Cancels: ${count.cancels}`)
+                    doc.text(`Total Minutes Holding the Wall: ${count.holdWall}`)
+                    doc.text('')
+                    doc.moveDown()
+                    doc.fontSize(12)
+                    doc.list(formattedLogs, { listType: 'bulletRadius', align: 'left' })
+                    doc.end()
+                    ws.on('finish', () => {
+                        // fs.readFile(`${__dirname}/../userGenPDFs/${userData.username}.pdf`, function (err, data) {
+                        //     res.contentType('application/pdf');
+                        //     res.setHeader('Content-disposition', `inline; filename="${userData.username}"`);
+                        //     res.send(data);
+                        // })
+                        res.render('displayPDF', {userData})
+                    })
+                    //res.sendfile(`${__dirname}/../userGenPDFs/${userData.username}.pdf`)
                 })
         })
 })
